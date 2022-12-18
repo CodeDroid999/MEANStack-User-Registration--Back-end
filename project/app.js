f37198b4583633a13ebba276f923c1a38282fe8a
CodeDroid999 <johnmwega2002@gmail.com>
2022-12-18 15:22:54 +0300

    requesting packages

--- project/app.js
+++ project/app.js
@@ -1,2 +1,6 @@
 require('./config/config');
 require('./models/db');
+
+const express= require('express'); 
+const bodyParser = require('body-parser');
+const cors = require('cors');
--- project/models/db.js
+++ project/models/db.js
@@ -3,6 +3,5 @@ const mongoose = require('mongoose');   //request mongoose
 mongoose.set("strictQuery", false);
 mongoose.connect(process.env.MONGODB_URI, (err)=> { 
      if (!err){console.log('MongoDb connection successfull.')}
-     else {console.log('MongoDb connection Error:' + JSON.stringify(err, undefined, 2));} 
-  
+     else {console.log('MongoDb connection Error:' + JSON.stringify(err, undefined, 2));}   
 });   //connect mongoose
