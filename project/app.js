6e22d2633b8e2ae9523e0112d1266f9a40ea55bc
CodeDroid999 <johnmwega2002@gmail.com>
2022-12-18 15:32:47 +0300

    start server

--- project/app.js
+++ project/app.js
@@ -1,6 +1,16 @@
 require('./config/config');
 require('./models/db');
 
+//requesting packages
 const express= require('express'); 
 const bodyParser = require('body-parser');
 const cors = require('cors');
+
+var app= express();
+
+//middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+//start server
+app.listen(process.env.PORT, () =>console.log('server started on port: ${process.env.PORT}'));
