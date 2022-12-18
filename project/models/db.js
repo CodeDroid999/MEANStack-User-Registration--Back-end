35aeafced8e4199ae9c13fbdad983e12d17cd2e2
CodeDroid999 <johnmwega2002@gmail.com>
2022-12-18 16:25:20 +0300

    modified models

--- project/models/db.js
+++ project/models/db.js
@@ -1,13 +1,8 @@
-//const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-//connect mongoose
 mongoose.connect(process.env.MONGODB_URI, (err) => {
     if (!err) { console.log('MongoDB connection succeeded.'); }
     else { console.log('Error in MongoDB connection : ' + JSON.stringify(err, undefined, 2)); }
 });
 
-//import  models
 require('./user.model');
-
-const mongoose = require('mongoose');
-
