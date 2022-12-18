f065d3d674cdab907d8b444d908790cbffb2f5ff
CodeDroid999 <johnmwega2002@gmail.com>
2022-12-18 15:40:36 +0300

    fixed indentation typo

--- project/models/db.js
+++ project/models/db.js
@@ -1,7 +1,8 @@
-const mongoose = require('mongoose');   //request mongoose
+//request mongoose
+const mongoose = require('mongoose');
 
-mongoose.set("strictQuery", false);
-mongoose.connect(process.env.MONGODB_URI, (err)=> { 
-     if (!err){console.log('MongoDb connection successfull.')}
-     else {console.log('MongoDb connection Error:' + JSON.stringify(err, undefined, 2));}   
-});   //connect mongoose
+//connect mongoose
+mongoose.connect(process.env.MONGODB_URI, (err) => {
+    if (!err) { console.log('MongoDB connection succeeded.'); }
+    else { console.log('Error in MongoDB connection : ' + JSON.stringify(err, undefined, 2)); }
+});
