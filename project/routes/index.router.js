0263d1e4adda1926281d2165f548199e3e2c7a95
CodeDroid999 <johnmwega2002@gmail.com>
2022-12-18 16:05:59 +0300

    configure routing

--- project/routes/index.router.js
+++ project/routes/index.router.js
@@ -1,2 +1,10 @@
 //requesting express package
 const express= require('express'); 
+
+const router = express.Router();
+
+const ctrlUser = require('../controllers/user.controller');
+
+router.post('/register', ctrlUser.register);
+
+module.exports = router;
