681ee5a24151f7fbaa53b590d0b91d19b403b3f7
CodeDroid999 <johnmwega2002@gmail.com>
2022-12-18 15:52:56 +0300

    added user schema and save to mongo

--- project/models/users.models.js
+++ project/models/users.models.js
@@ -1,2 +1,24 @@
 //request mongoose
 const mongoose = require('mongoose');
+
+//user schems
+var userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: 'Full name can\'t be empty'
+    },
+    email: {
+        type: String,
+        required: 'Email can\'t be empty',
+        unique: true
+    },
+    password: {
+        type: String,
+        required: 'Password can\'t be empty',
+        minlength : [4,'Password must be atleast 4 character long']
+    },
+    saltSecret: String
+});
+
+//saving the data to mongoose
+mongoose.model('User', userSchema);
