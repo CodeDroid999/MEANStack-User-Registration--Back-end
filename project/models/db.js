ceaa2aedc2319df1927ba915121b175fa1706aab
CodeDroid999 <johnmwega2002@gmail.com>
2022-12-21 17:40:42 +0300

    delete

--- project/models/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URI, (err) => {
-    if (!err) { console.log('MongoDB connection succeeded.'); }
-    else { console.log('Error in MongoDB connection : ' + JSON.stringify(err, undefined, 2)); }
-});
-
-require('./user.model');
