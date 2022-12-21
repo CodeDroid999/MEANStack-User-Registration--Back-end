1e3da5fc4adf2fe7e80fe8829ed863ca51712a04
CodeDroid999 <johnmwega2002@gmail.com>
2022-12-21 18:16:26 +0300

    require models

--- project/models/db.js
+++ project/models/db.js
@@ -6,3 +6,6 @@ mongoose.connect(process.env.MONGODB_URI, (err) => {
     if (!err) { console.log('MongoDB connection succeeded.'); }
     else { console.log('Error in MongoDB connection : ' + JSON.stringify(err, undefined, 2)); }
 });
+
+//require models
+require('./user.model');
