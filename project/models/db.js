387073e6a4c0ba3e4435f36e9cb4148797b4642f
CodeDroid999 <johnmwega2002@gmail.com>
2023-01-04 12:41:10 +0300

    establish connection

--- project/models/db.js
+++ project/models/db.js
@@ -1,2 +1,8 @@
 //require mongoose
 const mongoose = require('mongoose');
+
+//establish connection
+mongoose.connect(process.env.MONGODB_URI, (err) => {    
+    if (!err) { console.log('MongoDB connection succeeded.'); }
+    else { console.log('Error in MongoDB connection : ' + JSON.stringify(err, undefined, 2)); }
+});
