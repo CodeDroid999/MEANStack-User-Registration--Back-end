ec50dbc754d1676078e6d95ade3bd926966cf025
CodeDroid999 <johnmwega2002@gmail.com>
2023-01-04 13:11:13 +0300

    require packages

--- project/app.js
+++ project/app.js
@@ -1,1 +1,11 @@
+//require('./config/config');
+//require models
+//require('./models/db');
+
 require('./config/config');
+require('./models/db');
+
+//require packages
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
--- project/models/db.js
+++ project/models/db.js
@@ -1,10 +1,18 @@
 //require mongoose
-const mongoose = require('mongoose');
-//require models
-require('./models/db');
+//const mongoose = require('mongoose');
+
 
 //establish connection
-mongoose.connect(process.env.MONGODB_URI, (err) => {    
+/*mongoose.connect(process.env.MONGODB_URI, (err) => {    
+    if (!err) { console.log('MongoDB connection succeeded.'); }
+    else { console.log('Error in MongoDB connection : ' + JSON.stringify(err, undefined, 2)); }
+}); */
+
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI, (err) => {
     if (!err) { console.log('MongoDB connection succeeded.'); }
     else { console.log('Error in MongoDB connection : ' + JSON.stringify(err, undefined, 2)); }
 });
+
+
