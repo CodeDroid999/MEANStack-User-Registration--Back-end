3b46ad12b2cedd2169cb0ea0693eb56fe7b2c96e
CodeDroid999 <johnmwega2002@gmail.com>
2023-01-04 13:21:05 +0300

    variable to define userschema

--- project/models/users.models.js
+++ project/models/users.models.js
@@ -1,2 +1,20 @@
 //require mongoose
 const mongoose = require('mongoose');
+
+var userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: 'Full name can\'t be empty'
+    },
+    email: {
+        type: String,
+        required: 'Email can\'t be empty',
+        unique: true
+    },
+    password: {
+        type: String,
+        required: 'Password can\'t be empty',
+        minlength : [4,'Password must be atleast 4 character long']
+    },
+    saltSecret: String
+});
