28c6bf72534f892ec65e454b8723aba3b0b3b87f
CodeDroid999 <johnmwega2002@gmail.com>
2023-01-04 13:31:01 +0300

    add function to handle user requests

---
+++ project/controllers/user.controller.js
@@ -0,0 +1,20 @@
+
+
+//function to handle user requests
+module.exports.register = (req, res, next) => {
+    var user = new User();
+    user.fullName = req.body.fullName;
+    user.email = req.body.email;
+    user.password = req.body.password;
+    user.save((err, doc) => {
+        if (!err)
+            res.send(doc);
+        else {
+            if (err.code == 11000)
+                res.status(422).send(['Duplicate email adrress found.']);
+            else
+                return next(err);
+        }
+
+    });
+}
