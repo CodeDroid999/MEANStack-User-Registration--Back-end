cd8ff9053976fd7d83fdb31a06b327043f90206a
CodeDroid999 <johnmwega2002@gmail.com>
2023-01-04 15:13:36 +0300

    add error handler

--- project/app.js
+++ project/app.js
@@ -1,3 +1,37 @@
+require('./config/config');
+require('./models/db');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const rtsIndex = require('./routes/index.router');
+
+var app = express();
+
+// middleware
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/api', rtsIndex);
+
+// error handler
+app.use((err, req, res, next) => {
+    if (err.name === 'ValidationError') {
+        var valErrors = [];
+        Object.keys(err.errors).forEach(key => valErrors.push(err.errors[key].message));
+        res.status(422).send(valErrors)
+    }
+});
+
+// start server
+app.listen(process.env.PORT, () => console.log(`Server started at port : ${process.env.PORT}`));
+
+
+
+
+
+
+
 //require('./config/config');
 //require models
 //require('./models/db');
@@ -25,21 +59,3 @@ app.use('/api', rtsIndex);
 app.listen(process.env.PORT, () => console.log(`Server started at port : ${process.env.PORT}`)); 
  */
 
-require('./config/config');
-require('./models/db');
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
-const rtsIndex = require('./routes/index.router');
-
-var app = express();
-
-// middleware
-app.use(bodyParser.json());
-app.use(cors());
-app.use('/api', rtsIndex);
-
-// start server
-app.listen(process.env.PORT, () => console.log(`Server started at port : ${process.env.PORT}`));
