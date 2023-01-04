41469e591524d8c5d632fc955b01b5649dc4851d
CodeDroid999 <johnmwega2002@gmail.com>
2023-01-04 15:32:25 +0300

    call express server

--- project/app.js
+++ project/app.js
@@ -1,12 +1,15 @@
 require('./config/config');
 require('./models/db');
 
+//require packages
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+//require routing index
 const rtsIndex = require('./routes/index.router');
 
+//call express server
 var app = express();
 
 // middleware
@@ -25,37 +28,3 @@ app.use((err, req, res, next) => {
 
 // start server
 app.listen(process.env.PORT, () => console.log(`Server started at port : ${process.env.PORT}`));
-
-
-
-
-
-
-
-//require('./config/config');
-//require models
-//require('./models/db');
-/*
-require('./config/config');
-require('./models/db');
-
-//require packages
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
-//require routing
-const rtsIndex = require('./routes/index.router');
-
-//call express
-var app = express();
-
-// middleware
-app.use(bodyParser.json());
-app.use(cors());
-app.use('/api', rtsIndex);
-
-// start server
-app.listen(process.env.PORT, () => console.log(`Server started at port : ${process.env.PORT}`)); 
- */
-
