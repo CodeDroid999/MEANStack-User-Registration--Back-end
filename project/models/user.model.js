58e3dae6e48e12436b8b295b7ac0f3d3ef8920ae
CodeDroid999 <johnmwega2002@gmail.com>
2023-01-04 15:25:42 +0300

    add comments

--- project/models/user.model.js
+++ project/models/user.model.js
@@ -1,4 +1,6 @@
+//require mongoose
 const mongoose = require('mongoose');
+//require bcryptjs
 const bcrypt = require('bcryptjs');
 
 var userSchema = new mongoose.Schema({
@@ -36,6 +38,7 @@ userSchema.pre('save', function (next) {
     });
 });
 
+//register user schema in mongoose
 mongoose.model('User', userSchema);
 
 
@@ -44,61 +47,3 @@ mongoose.model('User', userSchema);
 
 
 
-/*
-//require mongoose
-const mongoose = require('mongoose');
-//require bcryptjs
-const bcrypt = require('bcryptjs');
-
-var userSchema = new mongoose.Schema({
-    fullName: {
-        type: String,
-        required: 'Full name can\'t be empty'
-    },
-    email: {
-        type: String,
-        required: 'Email can\'t be empty',
-        unique: true
-    },
-    password: {
-        type: String,
-        required: 'Password can\'t be empty',
-        minlength : [4,'Password must be atleast 4 character long']
-    },
-    saltSecret: String
-});
-
-
-
-//register user model in mongoose
-module.exports.register = (req, res, next) => {
-    var user = new User();
-    user.fullName = req.body.fullName;
-    user.email = req.body.email;
-    user.password = req.body.password;
-    user.save((err, doc) => {
-        if (!err)
-            res.send(doc);
-        else {
-            if (err.code == 11000)
-                res.status(422).send(['Duplicate email adrress found.']);
-            else
-                return next(err);
-        }
-
-    });
-}
-
-// Events
-userSchema.pre('save', function (next) {
-    bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(this.password, salt, (err, hash) => {
-            this.password = hash;
-            this.saltSecret = salt;
-            next();
-        });
-    });
-});
-
-mongoose.model('User', userSchema);
-*/
