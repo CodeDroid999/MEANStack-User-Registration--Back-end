6e0b6ceb268aa229e003bb2b316e23937a607076
CodeDroid999 <johnmwega2002@gmail.com>
2023-01-04 15:08:17 +0300

    uncommenting user.controller.js

--- project/controllers/user.controller.js
+++ project/controllers/user.controller.js
@@ -1,15 +1,3 @@
-/*
-//require mongoose
-const mongoose = require('mongoose');
-
-const User = mongoose.model('User');
-
-//function to handle user requests
-module.exports.register = (req, res, next) => {
-    console.log("inside register fn. ");
-}
-*/
-
 const mongoose = require('mongoose');
 
 const User = mongoose.model('User');
