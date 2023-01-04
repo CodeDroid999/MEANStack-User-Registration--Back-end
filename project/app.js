918a9d33b33943fca0dbd2ef84690ba437c96710
CodeDroid999 <johnmwega2002@gmail.com>
2023-01-04 13:50:18 +0300

    routing

--- project/app.js
+++ project/app.js
@@ -19,6 +19,7 @@ var app = express();
 // middleware
 app.use(bodyParser.json());
 app.use(cors());
+app.use('/api', rtsIndex);
 
 // start server
 app.listen(process.env.PORT, () => console.log(`Server started at port : ${process.env.PORT}`));
